6705e2f1cea350bbb74800712e1003e16f7167ce
nathanfaucett <nathanfaucett@gmail.com>
2016-02-13 20:42:56 -0600

    updates

--- test/adapter.js
+++ test/adapter.js
@@ -4,6 +4,6 @@ var rel = require("..");
 var adapter = exports;
 
 
-adapter.users = new rel.MemoryAdapter(),
-    adapter.organizations = new rel.MemoryAdapter(),
-    adapter.organizationMemberships = new rel.MemoryAdapter();
+adapter.users = new rel.MemoryAdapter();
+adapter.organizations = new rel.MemoryAdapter();
+adapter.organizationMemberships = new rel.MemoryAdapter();
